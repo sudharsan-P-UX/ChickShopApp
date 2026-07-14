75d46e4a42169c45efbc6394052d7bf54af13088
Sudharsan P <infofromarsha@gmail.com>
2026-07-15 00:28:11 +0530

    release7

--- chicken_shop_backend/src/middlewares/uploadMiddleware.js
+++ chicken_shop_backend/src/middlewares/uploadMiddleware.js
@@ -1,15 +1,6 @@
 const multer = require('multer');
-const path = require('path');
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, '../../uploads'));
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + '-' + file.originalname);
-  }
-});
-
+const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 module.exports = upload;
