de494d9c667cadbf3b1ca807823b5b86b789fd02
Sudharsan P <infofromarsha@gmail.com>
2026-07-15 10:51:14 +0530

    Disable Vercel body parser to allow file uploads

--- chicken_shop_backend/api/index.js
+++ chicken_shop_backend/api/index.js
@@ -1,3 +1,10 @@
 const app = require('../src/server');
 
 module.exports = app;
+
+// Disable built-in Vercel body parser to allow Multer to parse file uploads
+module.exports.config = {
+  api: {
+    bodyParser: false,
+  },
+};
